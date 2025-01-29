ebeea7b8b087ef2e3cc6f5adc9aa4f627bbab0c9
kevin cuadros <kevindavidcuadros@gmail.com>
2025-01-29 17:40:59 -0500

    Initial Commit

---
+++ Project-13-SwiftUI-instafilter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Project-13-SwiftUI-instafilter
+//
+//  Created by Kevin Cuadros on 29/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Project-13-SwiftUI-instafilter/Project_13_SwiftUI_instafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_13_SwiftUI_instafilterApp.swift
+//  Project-13-SwiftUI-instafilter
+//
+//  Created by Kevin Cuadros on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Project_13_SwiftUI_instafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
