bb9600220591b68aaf4a0583c9d526545095b4d1
kevin cuadros <kevindavidcuadros@gmail.com>
2025-02-04 15:29:15 -0500

    Loading photos from the user's photo library

--- Project-13-SwiftUI-instafilter/LoadPhotosUser.swift
+++ Project-13-SwiftUI-instafilter/LoadPhotosUser.swift
@@ -13,22 +13,61 @@ struct LoadPhotosUser: View {
     @State private var pickerImage: PhotosPickerItem?
     @State private var selectedImage: Image?
     
+    @State private var pickerImagesList = [PhotosPickerItem]()
+    @State private var selectedImagesList = [Image]()
+    
     var body: some View {
-        VStack {
-            
-            PhotosPicker(
-                "Select a picture",
-                selection: $pickerImage,
-                matching: .images
-            )
-            
-            selectedImage?
-                .resizable()
-                .scaledToFit()
-        }
-        .onChange(of: pickerImage) {
-            Task {
-                selectedImage = try await pickerImage?.loadTransferable(type: Image.self)
+        NavigationStack {
+            VStack {
+                
+                List {
+                    Section("One Item") {
+                        selectedImage?
+                            .resizable()
+                            .scaledToFit()
+                    }
+                    
+                    Section("Many Items") {
+                        ForEach(0..<selectedImagesList.count, id: \.self) { i in
+                            selectedImagesList[i]
+                                .resizable()
+                                .scaledToFit()
+                        }
+                    }
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    PhotosPicker(
+                        "Select a picture",
+                        selection: $pickerImage,
+                        matching: .images
+                    )
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    PhotosPicker(
+                        "Select a picture list",
+                        selection: $pickerImagesList,
+                        maxSelectionCount: 3,
+                        matching: .images
+                    )
+                }
+            }
+            .onChange(of: pickerImage) {
+                Task {
+                    selectedImage = try await pickerImage?.loadTransferable(type: Image.self)
+                }
+            }
+            .onChange(of: pickerImagesList) {
+                selectedImagesList.removeAll()
+                Task {
+                    for item in pickerImagesList {
+                        if let image = try await item.loadTransferable(type: Image.self) {
+                            selectedImagesList.append(image)
+                        }
+                    }
+                }
             }
         }
     }
